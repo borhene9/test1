7347e9268037561e24372ff4625f6b1ca44b9d87
borhene9 <nejaiborheneeddine@isg.u-tunis.tn>
2025-04-28 17:41:32 +0100

    Try 5 update db url and disabling rules changing port

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    PORT: process.env.PORT || "3000",
+  
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    PORT: process.env.PORT || 3000,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    PORT: process.env.PORT || 3000,
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,78 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+
+const authOptions: AuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(
+        credentials: Record<"username" | "password", string> | undefined,
+        req: any
+      ) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            username: credentials.username,
+          },
+          include: {
+            role: true,
+          },
+        });
+
+        if (!user) {
+          throw new Error("Invalid username or password");
+        }
+
+        const passwordMatch = await bcrypt.compare(credentials.password, user.password);
+
+        if (!passwordMatch) {
+          throw new Error("Invalid username or password");
+        }
+
+        return {
+          id: user.id,
+          username: user.username,
+          role: user.role.name,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    jwt: async ({ token, user, account, profile, trigger }: { token: any; user?: any, account: any, profile?: any, trigger?: any }) => {
+      if (user) {
+        token.username = user.username;
+        token.id = user.id;
+        token.role = user.role;
+        console.log('JWT token:', token);
+      }
+      return token;
+    },
+    session: async ({ session, token }: { session: any; token: any }) => {
+      if (token) {
+        session.user.username = token.username;
+        session.user.id = token.id;
+        session.user.role = token.role;
+        console.log('Session:', session);
+      }
+      return session;
+    },
+  },
+};
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
