b4e0b76674b0d3ab24a9fbcaed156fd62f5f958f
borhene9 <nejaiborheneeddine@isg.u-tunis.tn>
2025-04-28 18:17:08 +0100

    Try 10

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    PORT: process.env.PORT || '3000',
-  },
 };
 
 export default nextConfig;
