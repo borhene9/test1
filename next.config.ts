e3f968449b91262b6a2fa3ac2650c3d3e85c7e13
borhene9 <nejaiborheneeddine@isg.u-tunis.tn>
2025-04-28 17:50:07 +0100

    Try 6

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    PORT: process.env.PORT || 3000,
+  env: {
+    PORT: process.env.PORT || '3000',
   },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    PORT: process.env.PORT || 3000,
+  server: {
+    port: parseInt(process.env.PORT || '3000', 10),
   },
 };
 
